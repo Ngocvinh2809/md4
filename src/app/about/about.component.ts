5e56a2054d907f3bdb7679733979f35a0b0cbcd6
Ngocvinh2809 <vinhtrann2809@gmail.com>
2022-10-12 16:26:29 +0700

    up

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,5 +11,10 @@ export class AboutComponent implements OnInit {
   a="2020/10/11";
   ngOnInit(): void {
   }
+  birthday = new Date(1988, 3, 15); // April 15, 1988
+  toggle = true; // start with true == shortDate
+
+  get format()   { return this.toggle ? 'shortDate' : 'fullDate'; }
+  toggleFormat() { this.toggle = !this.toggle; }
 
 }
